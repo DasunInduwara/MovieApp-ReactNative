b4426f919e2a412632c7e76396be0a7e5ac879ac
Dasun Perera <dasuniduwara2@gmail.com>
2024-04-24 22:47:37 +0530

    map screen added

--- ios/MovieApp/AppDelegate.mm
+++ ios/MovieApp/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -6,6 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBV0YPtxasM8_R1uXv1tOw_WqTGXB_DGdk"];
   self.moduleName = @"MovieApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
